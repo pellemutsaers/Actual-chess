23f097b8fda7dec404243bf0fa7144307370574a
pellemutsaers <pelle@peterester.nl>
2022-05-11 12:58:28 +0200

    FEN support

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ WIDTH, HEIGHT = 800, 800
 WHITE, BLACK = (235, 210, 180), (115, 85, 70)
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 START_POSITION = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+RANDOM_POSITION = "7r/1P2p3/3bB2N/3K2pp/4P3/5PR1/kP2pP2/8 w KQkq - 0 1"
 
 #* Pieces
 blackRook = pygame.image.load("Chess pieces/black-rook.png")
@@ -14,12 +15,12 @@ blackQueen = pygame.image.load("Chess pieces/black-queen.png")
 blackKing = pygame.image.load("Chess pieces/black-king.png")
 blackPawn = pygame.image.load("Chess pieces/black-pawn.png")
 
-#whiteRook = pygame.image.load("Chess pieces/white-rook.png")
-#whiteKnight = pygame.image.load("Chess pieces/white-knight.png")
-#whiteBishop = pygame.image.load("Chess pieces/white-bishop.png")
-#whiteQueen = pygame.image.load("Chess pieces/white-queen.png")
-#whiteKing = pygame.image.load("Chess pieces/white-king.png")
-#whitePawn = pygame.image.load("Chess pieces/white-pawn.png")
+whiteRook = pygame.image.load("Chess pieces/white-rook.png")
+whiteKnight = pygame.image.load("Chess pieces/white-knight.png")
+whiteBishop = pygame.image.load("Chess pieces/white-bishop.png")
+whiteQueen = pygame.image.load("Chess pieces/white-queen.png")
+whiteKing = pygame.image.load("Chess pieces/white-king.png")
+whitePawn = pygame.image.load("Chess pieces/white-pawn.png")
 
 def splitChar(row):
     return [char for char in row]
@@ -43,31 +44,27 @@ class Board:
 
     def drawPieces(self, position, redecodePosition):
         global pieceList2d
+        global settings
+        
         if redecodePosition:
-            pieceList2d = self.makePieceList(position)
+            pieceList2d, settings = self.makePieceList(position)
 
         for column, row in enumerate(pieceList2d):
             for index, piece in enumerate(row):
                 if piece == " ":
                     pass
                 elif piece == "P":
-                    #WINDOW.blit(whitePawn, ((index)*WIDTH / 8, (column)*WIDTH / 8))
-                    pass
+                    WINDOW.blit(pygame.transform.smoothscale(whitePawn, (WIDTH / 8, WIDTH / 8)), (self.offsetX / 2 + index * WIDTH / 8, self.offsetY / 2 + column * HEIGHT / 8))
                 elif piece == "R":
-                    #WINDOW.blit(whiteRook, ((index)*WIDTH / 8, (column)*WIDTH / 8))
-                    pass
+                    WINDOW.blit(pygame.transform.smoothscale(whiteRook, (WIDTH / 8, WIDTH / 8)), (self.offsetX / 2 + index * WIDTH / 8, self.offsetY / 2 + column * HEIGHT / 8))
                 elif piece == "N":
-                    #WINDOW.blit(whiteKnight, ((index)*WIDTH / 8, (column)*WIDTH / 8))
-                    pass
+                    WINDOW.blit(pygame.transform.smoothscale(whiteKnight, (WIDTH / 8, WIDTH / 8)), (self.offsetX / 2 + index * WIDTH / 8, self.offsetY / 2 + column * HEIGHT / 8))
                 elif piece == "B":
-                    #WINDOW.blit(whiteBishop, ((index)*WIDTH / 8, (column)*WIDTH / 8))
-                    pass
+                    WINDOW.blit(pygame.transform.smoothscale(whiteBishop, (WIDTH / 8, WIDTH / 8)), (self.offsetX / 2 + index * WIDTH / 8, self.offsetY / 2 + column * HEIGHT / 8))
                 elif piece == "Q":
-                    #WINDOW.blit(whiteQueen, ((index)*WIDTH / 8, (column)*WIDTH / 8))
-                    pass
+                    WINDOW.blit(pygame.transform.smoothscale(whiteQueen, (WIDTH / 8, WIDTH / 8)), (self.offsetX / 2 + index * WIDTH / 8, self.offsetY / 2 + column * HEIGHT / 8))
                 elif piece == "K":
-                    #WINDOW.blit(whiteKing, ((index)*WIDTH / 8, (column)*WIDTH / 8))
-                    pass
+                    WINDOW.blit(pygame.transform.smoothscale(whiteKing, (WIDTH / 8, WIDTH / 8)), (self.offsetX / 2 + index * WIDTH / 8, self.offsetY / 2 + column * HEIGHT / 8))
                 elif piece == "p":
                     WINDOW.blit(pygame.transform.smoothscale(blackPawn, (WIDTH / 8, WIDTH / 8)), (self.offsetX / 2 + index * WIDTH / 8, self.offsetY / 2 + column * HEIGHT / 8))
                 elif piece == "r":
@@ -95,14 +92,13 @@ class Board:
                 if piece.isdigit():
                     row.pop(j)
                     for k in range(int(piece)):
-                        row.insert(k, " ")
-
-        return pieceList2d
+                        row.insert(k + j, " ")
+        return pieceList2d, position[-5::1]
 
 def main():
     BOARD = Board()
     BOARD.drawBoard()
-    BOARD.drawPieces(START_POSITION, True)
+    BOARD.drawPieces(RANDOM_POSITION, True)
     running = True
 
     while running:
